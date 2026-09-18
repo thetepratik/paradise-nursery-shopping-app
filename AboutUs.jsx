e221428b01f62ce171f13753453a83a0d2845559
Pratik Thete <pratikthete77@gmail.com>
2026-09-18 10:12:26 +0530

    Create AboutUs component for Paradise Nursery
    Added an AboutUs component that describes Paradise Nursery's mission and offerings.

---
+++ AboutUs.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+
+function AboutUs() {
+return ( <div className="about-us"> <h1>About Paradise Nursery</h1>
+
+```
+  <p>
+    Paradise Nursery is an online plant shop dedicated to bringing
+    beautiful and healthy houseplants to plant lovers. We offer a wide
+    variety of indoor plants suitable for homes, offices, and other
+    indoor spaces.
+  </p>
+
+  <p>
+    Our mission is to make it easy for everyone to bring the beauty of
+    nature into their everyday lives. We provide quality plants at
+    affordable prices along with a simple and convenient online shopping
+    experience.
+  </p>
+
+  <h2>Why Choose Paradise Nursery?</h2>
+
+  <ul>
+    <li>Wide variety of houseplants</li>
+    <li>Healthy and quality plants</li>
+    <li>Affordable prices</li>
+    <li>Easy online shopping experience</li>
+    <li>Plants suitable for homes and offices</li>
+  </ul>
+
+  <p>
+    At Paradise Nursery, we believe that every space can become greener
+    and more beautiful with the right plant.
+  </p>
+</div>
+```
+
+);
+}
+
+export default AboutUs;
